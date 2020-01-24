ada9a2830fb4f8b740158c23b6e53d9de6f07374
Faisal-Munir <engr.faisal2018@gmail.com>
2020-01-24 19:28:14 +0500

    as "main.rs"

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod Pakistan{
+  pub mod Punjab{
+       pub fn punjab(){
+            println!("Punjab is a provice of Pakistan");
+        } 
+
+  }
+
+}
+
+
+fn main() {
+
+    Pakistan::Punjab::punjab();
+    
+}
